fe41b7ebef5b320f63a466ff5fdbd659a7175e95
vdnq <valery.danilik@gmail.com>
2016-12-27 23:23:41 +0300

    news added on the first page

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, render_to_response
 from datetime import datetime
+from news.models import NewsFeed
 
 # Create your views here.
 
 def index(request):
-    return render_to_response('index.html', {'time': datetime.now()})
+    newsfeed = NewsFeed.objects.all()
+    return render_to_response('index.html', {'time': datetime.now(), 'news': newsfeed})
 
 def portfolio(request):
     return render_to_response('portfolio.html', {'time': datetime.now()})
--- news/models.py
+++ news/models.py
@@ -4,5 +4,5 @@ from django.utils import timezone
 class NewsFeed(models.Model):
     company_id = models.CharField(max_length=6)
     added_at = models.DateTimeField(default=timezone.now)
-    news_short = models.TextField(default="NULL")
-    news_url = models.URLField(unique=True, default=r'http:\\www.finoggr.com')
+    news_short = models.TextField(blank=True)
+    news_url = models.URLField(unique=True)
