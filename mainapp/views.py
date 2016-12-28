000eed60cb98265c3deff88606c459ac2bfd0a03
vdnq <valery.danilik@gmail.com>
2016-12-28 15:05:03 +0300

    some corrections for news feature

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,7 +5,7 @@ from news.models import NewsFeed
 # Create your views here.
 
 def index(request):
-    newsfeed = NewsFeed.objects.all()
+    newsfeed = NewsFeed.objects.all().order_by("id")[::-1]
     return render_to_response('index.html', {'time': datetime.now(), 'news': newsfeed})
 
 def portfolio(request):
